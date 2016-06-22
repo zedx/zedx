92a7f074c187387a84947be14d5233df7df517ef
zorx <oudjehih@zedx.org>
2016-06-22 17:16:59 +0200

    add footer menus

--- themes/Default/views/templates/partials/footer.blade.php
+++ themes/Default/views/templates/partials/footer.blade.php
@@ -1,14 +1,28 @@
-<!-- Footer -->
-<footer>
-    <div class="row">
-        <div class="col-lg-12">
-            <p>
-            <span class="pull-left">Copyright &copy; <a href="{{ setting('website_url') }}">{{ setting('website_name') }}</a> {{ \Carbon\Carbon::now()->format("Y") }}</span>
-            <span class="pull-right">
-              <i class="fa fa-heart" style="color:#dd4b39"></i> {!! trans('frontend.zedx.credits') !!}
-            </span>
-            </p>
+<footer class="text-center">
+    {{--*/ $_footerMenus = renderMenu('footer') /*--}}
+    @if ($_footerMenus)
+    <div class="footer-above">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul>
+                        {!! $_footerMenus !!}
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
-    <!-- /.row -->
+    @endif
+    <div class="footer-below">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <span class="pull-left">Copyright &copy; <a href="{{ setting('website_url') }}">{{ setting('website_name') }}</a> {{ \Carbon\Carbon::now()->format("Y") }}</span>
+                    <span class="pull-right">
+                      <i class="fa fa-heart" style="color:#dd4b39"></i> {!! trans('frontend.zedx.credits') !!}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
