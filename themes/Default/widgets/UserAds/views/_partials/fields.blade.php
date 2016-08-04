9a70a24fb09d8065f49b41e455c4b125cdde3dd2
zorx <oudjehih@zedx.org>
2016-08-04 12:16:15 +0200

    fix widget name

--- themes/Default/widgets/UserAds/views/_partials/fields.blade.php
+++ themes/Default/widgets/UserAds/views/_partials/fields.blade.php
@@ -2,8 +2,8 @@
 
 @foreach($ad->category->fields()->whereIsInAdsList(true)->get() as $field)
   @if(in_array($field->type, [1,2,3]))
-    @include('widget_frontend_theme_searchadslist::_partials.selectField')
+    @include('widget_frontend_theme_userads::_partials.selectField')
   @elseif(in_array($field->type, [4,5]))
-    @include('widget_frontend_theme_searchadslist::_partials.inputField')
+    @include('widget_frontend_theme_userads::_partials.inputField')
   @endif
 @endforeach
