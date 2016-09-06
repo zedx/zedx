6ea5fd41c82d1f559c025e944161dc74f960b799
zorx <oudjehih@zedx.org>
2016-09-06 11:29:52 +0200

    Add TinyMCE plugins
    Connects to zedx/core#71

--- widgets/Frontend/ZEDx/Editor/views/edit.blade.php
+++ widgets/Frontend/ZEDx/Editor/views/edit.blade.php
@@ -83,7 +83,15 @@
       var setHtmlEditor = function() {
         tinymce.init({
           selector: '#' + selector,
-          plugins: "autoresize",
+          plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools'
+          ],
+          toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+          toolbar2: 'print preview media | forecolor backcolor emoticons',
+          image_advtab: true
         });
       }
 
