37308b12de7b2efd818366bf2f14fa853aecd4b6
zorx <oudjehih@zedx.org>
2016-11-07 16:30:58 +0100

    Add lang to backend

---
+++ themes/Default/lang/fr/backend.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'customize_theme' => [
+        'style'         => [
+            'customize_styles'      => 'Personnaliser les styles',
+            'customize_styles_help' => "Personnalisez l'apparence de votre site en ajoutant votre propre code CSS qui sera appliqué en priorité par rapport aux styles par défaut de ZEDx.",
+            'edit_style'            => 'Éditer le CSS personnalisé',
+        ],
+        'navbar' => [
+            'navbar' => 'Barre de navigation',
+            'default_background' => 'Fond',
+            'active_background' => 'Fond actif',
+            'default_text' => 'Texte',
+            'active_text' => 'Texte actif',
+        ],
+        'buttons' => [
+            'buttons' => 'Boutons',
+            'primary' => 'Barre de navigation',
+            'navbar' => 'Barre de navigation',
+        ],
+        'colors'        => [
+            'colors'     => 'Couleurs',
+            'background' => "Arrière plan",
+            'blocks'     => 'Blocs',
+            'primary'    => 'Couleur primaire',
+            'secondary'  => 'Couleur secondaire',
+        ],
+        'validate'      => "Valider",
+        'apply_updates' => "Appliquer les modifications",
+    ],
+];
