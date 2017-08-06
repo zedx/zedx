e8ba15001e5689a0a52f715b408f8ce424d91ec1
zorx <oudjehih@zedx.org>
2017-08-06 17:07:44 +0200

    add share buttons
    connects to #33

--- themes/Default/widgets/AdDetails/views/index.blade.php
+++ themes/Default/widgets/AdDetails/views/index.blade.php
@@ -145,4 +145,13 @@
           </div>
         </div>
     </div>
+    <div class="panel-footer">
+      <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+      <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+      <a class="a2a_button_facebook"></a>
+      <a class="a2a_button_twitter"></a>
+      <a class="a2a_button_google_plus"></a>
+      </div>
+      <script async src="https://static.addtoany.com/menu/page.js"></script>
+    </div>
 </div>
